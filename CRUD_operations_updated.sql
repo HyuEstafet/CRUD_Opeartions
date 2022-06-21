4dc20406d670c27bea7f528b1f2393b1f75c8d0c
HyuEstafet <hyulya.gyuler@estafet.com>
2022-06-22 00:36:57 +0300

    updated CRUD operations tasks

--- CRUD_operations_updated.sql
+++ CRUD_operations_updated.sql
@@ -4,6 +4,11 @@
 INSERT INTO customers (customer_id,customer_name,customer_email,customer_phone,customer_age,gdpr_status,customer_profile_status,date_profile_created,date_profile_deactivated,deactivation_reason,customer_notes)
 VALUES (001,'Nick Clements','nick@email.com','+44765982735',34,TRUE,TRUE,CURRENT_TIMESTAMP,NULL,NULL,'Just an example text');
 
+-- Create customer with all fields (trying it after mentor's review)
+INSERT INTO customers (customer_id,customer_name,customer_email,customer_phone,customer_age,gdpr_status,customer_profile_status,date_profile_created,date_profile_deactivated,deactivation_reason,customer_notes)
+VALUES (005,'Abby Simons','abbys@email.com','+44775982735',40,TRUE,TRUE,CURRENT_TIMESTAMP,NULL,NULL,'Just an example text for Abby');
+
+
 --Create customer with the mandatory fields
 INSERT INTO customers (customer_id,customer_name,customer_email,customer_phone,gdpr_status,customer_profile_status)
 VALUES (002,'Federica Silvi','feds@email.com','+44745906545',FALSE,TRUE)
@@ -18,7 +23,8 @@ SELECT customer_name,customer_phone FROM customers;
 SELECT gdpr_status,customer_profile_status,date_profile_created,date_profile_deactivated,deactivation_reason,customer_notes FROM customers;
 
 --Retrieving all customer info by customer id
-SELECT customer_id FROM customers;
+SELECT * FROM customers
+WHERE customer_id=001;
 
 --Updating customer by id--
 
@@ -29,13 +35,13 @@ WHERE customer_id = 002;
 
 --Deactivate account
 UPDATE customers 
-SET customer_profile_status = FALSE
-WHERE customer_id = 001;
+SET customer_profile_status = FALSE,date_profile_deactivated=now(),deactivation_reason='The customer is no more willing to share any personal info'
+WHERE customer_id = 005;
 
 --Activate account
 UPDATE customers 
-SET customer_profile_status = TRUE
-WHERE customer_id = 001;
+SET customer_profile_status = TRUE,date_profile_deactivated=NULL,deactivation_reason=NULL,customer_notes = 'The customer changed their mind'
+WHERE customer_id = 005;
 
 --Delete customer by id 
 DELETE FROM customers
